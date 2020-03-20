14ef3bbb9cb6728bc63ae7b99ad477e452141a2c
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2020-03-20 12:42:35 +0700

    Parse fixed waning cast type int vs size_t

--- make-service.sh
+++ make-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WORKDIR="/tmp/repos"
+WORKDIR="/home/mrnhat/repos"
 
 mkdir -p $WORKDIR
 
@@ -22,18 +22,35 @@ else
 	git pull
 fi
 
-mkdir -p $WORKDIR/service
+if [ ! -d "$WORKDIR/service" ]
+then
+	mkdir -p $WORKDIR/service
+else
+	rm -rf $WORKDIR/service/*
+fi
 
 cp -rf $WORKDIR/cpp-httplib/httplib.h $WORKDIR/service/
 cat $WORKDIR/RSJp-cpp/RSJparser.tcc > $WORKDIR/service/json.h
 
 sed -i 's/RSJ/JSON/g' $WORKDIR/service/json.h
+#sed -i 's/int /size_t /g' $WORKDIR/service/json.h
+#sed -i 's/int\*/size_t*/g' $WORKDIR/service/json.h
+#sed -i 's/new int/new size_t/g' $WORKDIR/service/json.h
+#sed -i 's/size_t  JSONresource::as<int>/int JSONresource::as<int>/g' $WORKDIR/service/json.h
+sed -i 's/int b=0/size_t b=0/g' $WORKDIR/service/json.h
+sed -i 's/int a=0/size_t a=0/g' $WORKDIR/service/json.h
+sed -i 's/int newline_pos =/size_t newline_pos =/g' $WORKDIR/service/json.h
+sed -i 's/int\* parse_start_str_pos/size_t* parse_start_str_pos/g' $WORKDIR/service/json.h
+sed -i 's/parse_start_str_pos = new int;/parse_start_str_pos = new size_t;/g' $WORKDIR/service/json.h
+sed -i 's/operator\[\] (int indx)/operator[] (size_t indx)/g' $WORKDIR/service/json.h
 
 cat > $WORKDIR/service/service.cpp <<EOF
 #include <iostream>
 #include <string>
 #include "httplib.h"
 #include "json.h"
+
+using namespace httplib;
 
 JSONresource *json_decode(std::string);
 std::string json_encode(JSONresource);
@@ -45,6 +62,11 @@ int main(int argc, char **argv) {
     } else {
         std::cout << "Error parse JSON" << std::endl;
     }
+    Server svr;
+    svr.Get("/", [](const Request& req, Response& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+    svr.listen("127.0.0.1", 9600);
     return 0;
 }
 
@@ -57,5 +79,5 @@ std::string json_encode(JSONresource src) {
 EOF
 
 cd $WORKDIR/service
-g++ -std=c++11 -Wall service.cpp -o service
+g++ -std=c++11 -Wall service.cpp -o service -lpthread
 ./service
