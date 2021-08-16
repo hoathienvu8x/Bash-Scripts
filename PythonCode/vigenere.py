fea1b4d0c5ac529752b9779231fb62a5d1d7b2e6
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2021-08-16 20:30:33 +0700

    Create vigenere.py

---
+++ PythonCode/vigenere.py
@@ -0,0 +1,19 @@
+def cipher_encrypt(plaintext, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    plaintext_int = [ord(i) for i in plaintext]
+    ciphertext = ''
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_length])
+        ciphertext += chr(value)
+    return ciphertext
+
+def cipher_decrypt(ciphertext, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    ciphertext_int = [ord(i) for i in ciphertext]
+    plaintext = ''
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_as_int[i % key_length])
+        plaintext += chr(value)
+    return plaintext
