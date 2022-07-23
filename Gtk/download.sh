2d58d188b67039be793ac97e366d870b364dd15d
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2022-07-23 09:43:38 +0700

    Create download.sh

---
+++ Gtk/download.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ ! -d "html" ]; then
+    mkdir html
+fi
+
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut.html" -O "html/gtk_tut.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-1.html" -O "html/gtk_tut-1.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-2.html" -O "html/gtk_tut-2.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-3.html" -O "html/gtk_tut-3.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-4.html" -O "html/gtk_tut-4.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-5.html" -O "html/gtk_tut-5.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-6.html" -O "html/gtk_tut-6.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-7.html" -O "html/gtk_tut-7.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-8.html" -O "html/gtk_tut-8.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-9.html" -O "html/gtk_tut-9.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-10.html" -O "html/gtk_tut-10.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-11.html" -O "html/gtk_tut-11.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-12.html" -O "html/gtk_tut-12.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-13.html" -O "html/gtk_tut-13.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-14.html" -O "html/gtk_tut-14.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-15.html" -O "html/gtk_tut-15.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-16.html" -O "html/gtk_tut-16.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-17.html" -O "html/gtk_tut-17.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-18.html" -O "html/gtk_tut-18.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-19.html" -O "html/gtk_tut-19.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-20.html" -O "html/gtk_tut-20.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-21.html" -O "html/gtk_tut-21.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-22.html" -O "html/gtk_tut-22.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-23.html" -O "html/gtk_tut-23.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-24.html" -O "html/gtk_tut-24.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-25.html" -O "html/gtk_tut-25.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-26.html" -O "html/gtk_tut-26.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-27.html" -O "html/gtk_tut-27.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-28.html" -O "html/gtk_tut-28.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-29.html" -O "html/gtk_tut-29.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-30.html" -O "html/gtk_tut-30.html"
+wget -q "http://www.mit.edu/afs.new/sipb/project/gtk/gtk_v1.2/tutorial/html/gtk_tut-31.html" -O "html/gtk_tut-31.html"
+
+exit
