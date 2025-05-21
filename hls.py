a611a28296c7defbc3220a2e649f46196fcc6b37
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2025-05-21 22:30:35 +0700

    Update hls.py

--- hls.py
+++ hls.py
@@ -10,16 +10,21 @@ if __name__ == '__main__':
 
   fw = open('hls.m3u8','w')
   fs = open('stream.sh','w')
+  ft = open('list.txt','w')
   lines = text.split('\n')
   i = 1
   for line in lines:
     if 'https://' in line:
       name = 'seg-%d' % i
-      print("wget --inet4-only '%s' -O '%s'" % (line, name))
-      fw.write("%s\n" % name)
-      fs.write("dd if=%s of=%s.ts bs=1 skip=8\n" % (name, name))
+      print("echo 'Download %s ...'\nwget -q --inet4-only '%s' -O '%s'" % (name, line, name))
+      fw.write("%s.ts\n" % name)
+      fs.write("echo 'build %s.ts'\ndd if=%s of=%s.ts bs=1 skip=8\nrm -rf '%s'\n" % (name, name, name, name))
+      ft.write("file '%s.ts'\n" % name)
       i += 1
     else:
       fw.write("%s\n" % line)
   fw.close()
   fs.close()
+  ft.close()
+
+print('ffmpeg -f concat -safe 0 -i list.txt -c copy output.mp4')
